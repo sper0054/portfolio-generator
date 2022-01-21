32f0067e4678b0ea93ab550eee006d6ed6759d6d
sper0054 <teresa.sperl@gmail.com>
2022-01-21 15:49:16 -0600

    first question

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+//const pageHTML = generatePage(name, github):
 
-const [name, github] = profileDataArgs;
+//fs.writeFile('./index.html', pageHTML, err => {
+   // if (err) throw err;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-//const printProfileData = profileDataArr => {
-    //This...
-    //for (let i = 0; i < profileDataArr.length; i += 1) {
-        //console.log(profileDataArr[i]);
-    //}
-
-    //console.log('================');
-
-    //Is the same as this...
-    //profileDataArr.forEach(profileItem => console.log(profileItem));
-//};
-
-//printProfileData(profileDataArgs);
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
