d357c7a91884f4b4483194d25479faa1b58c98f8
sper0054 <teresa.sperl@gmail.com>
2022-01-22 14:52:11 -0600

    page outlined

--- app.js
+++ app.js
@@ -129,12 +129,12 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
-    //will be uncommented in lesson 4
-    //const pageHTML = generatePage(portfolioData);
-    //fs.writeFile('./index.html', pageHTML, err => {
-   // if (err) throw new Error (err);
-    //console.log('Portfolio complete! Check out index.html to see the output!');
-    //});
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error (err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
